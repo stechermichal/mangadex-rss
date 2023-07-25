b0756a57887e72bc9e739a8d61fdebf06b6faf27
Michal Štecher <m.stecher@cleverbee.com>
2023-07-25 21:33:47 +0200

    feat: add run scripts for windows/linux to make it a bit easier

---
+++ run.bat
@@ -0,0 +1,44 @@
+@echo off
+SETLOCAL
+
+:: Check if Node.js is installed
+node -v >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo Node.js is not installed. Please install it from https://nodejs.org/ and run this script again.
+    EXIT /B 1
+)
+
+:: Check if npm is installed
+npm -v >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo npm is not installed. Please install it from https://nodejs.org/ and run this script again.
+    EXIT /B 1
+)
+
+:: Install necessary packages
+echo Installing necessary packages...
+npm install
+IF ERRORLEVEL 1 (
+    echo An error occurred while installing the packages.
+    EXIT /B 1
+)
+
+:: Compile TypeScript to JavaScript
+echo Compiling TypeScript to JavaScript...
+npx tsc
+IF ERRORLEVEL 1 (
+    echo An error occurred while compiling TypeScript.
+    EXIT /B 1
+)
+
+:: Run the JavaScript code
+echo Running JavaScript code...
+node index.js
+IF ERRORLEVEL 1 (
+    echo An error occurred while running the JavaScript code.
+    EXIT /B 1
+)
+
+echo Success! Your feed URLs have been generated.
+
+ENDLOCAL
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Check if Node.js is installed
+if ! command -v node >/dev/null 2>&1; then
+    echo "Node.js is not installed. Please install it from https://nodejs.org/ and run this script again."
+    exit 1
+fi
+
+# Check if npm is installed. If the user has installed Node.js, they should have npm as well, but this provides more specific error message if they somehow don't.
+if ! command -v npm >/dev/null 2>&1; then
+    echo "npm is not installed. Please install it from https://nodejs.org/ and run this script again."
+    exit 1
+fi
+
+# Install project dependencies
+echo "Installing project dependencies..."
+npm install
+if [ $? -ne 0 ]; then
+    echo "An error occurred while installing the project dependencies."
+    exit 1
+fi
+
+# Compile TypeScript to JavaScript
+echo "Compiling TypeScript to JavaScript..."
+npx tsc
+if [ $? -ne 0 ]; then
+    echo "An error occurred while compiling TypeScript."
+    exit 1
+fi
+
+# Run the JavaScript code
+echo "Running JavaScript code..."
+node index.js
+if [ $? -ne 0 ]; then
+    echo "An error occurred while running the JavaScript code."
+    exit 1
+fi
+
+echo "Success! Your feed URLs have been generated."
