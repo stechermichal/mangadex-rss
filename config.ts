a8572fcc40c2144622cc7836bb5bd517489c9cd8
Michal Štecher <m.stecher@cleverbee.com>
2023-07-25 20:10:55 +0200

    feat: no longer create mangaIds.json, create feedUrlsList.txt for easy pasting, move hardcoded stuff to config.ts

---
+++ config.ts
@@ -0,0 +1,7 @@
+export default {
+    BASE_URL: 'https://mdrss.tijlvdb.me/feed?',
+    MANGA_API: 'https://api.mangadex.org',
+    DEFAULT_LANGUAGE: 'en',
+    USERNAME: 'replaceWithYourUsername',
+    PASSWORD: 'replaceWithYourPassword',
+};
--- index.ts
+++ index.ts
@@ -1,25 +1,33 @@
-const axios = require('axios');
-const fs = require('fs');
+import axios from 'axios';
+import fs from 'fs';
+import CONFIG from './config';
 
-const baseUrl = 'https://mdrss.tijlvdb.me/feed?';
-const language = 'en'; // Change this to the language you want
-
-async function authenticate() {
-    const result = await axios.post('https://api.mangadex.org/auth/login', {
-        username: 'temp',
-        password: 'temp'
-    });
-    return { token: result.data.token.session, refreshToken: result.data.token.refresh }; // Return as an object
+interface Manga {
+    id: string;
+    title: string;
 }
 
-async function getAllMangaId(token: string, refreshToken: string) {
-    const mangaIds = [];
-    let limit = 100;
+interface TokenResponse {
+    token: string;
+    refreshToken: string;
+}
+
+async function authenticate(): Promise<TokenResponse> {
+    const result = await axios.post(`${CONFIG.MANGA_API}/auth/login`, {
+        username: CONFIG.USERNAME,
+        password: CONFIG.PASSWORD
+    });
+    return { token: result.data.token.session, refreshToken: result.data.token.refresh };
+}
+
+async function getAllMangaId(token: string, refreshToken: string): Promise<Manga[]> {
+    const mangaIds: Manga[] = [];
+    let limit = 10;
     let offset = 0;
     let total = 0;
 
     do {
-        const result = await axios.get('https://api.mangadex.org/user/follows/manga', {
+        const result = await axios.get(`${CONFIG.MANGA_API}/user/follows/manga`, {
             headers: {
                 Authorization: `Bearer ${token}`
             },
@@ -32,30 +40,26 @@ async function getAllMangaId(token: string, refreshToken: string) {
         total = result.data.total;
         for (const manga of result.data.data) {
             const title = await getMangaTitleById(manga.id, token);
+            console.log(`Fetched title for manga with id ${manga.id}: ${title}`);
             mangaIds.push({ id: manga.id, title });
         }
 
         offset += limit;
-        console.log(`Progress: ${offset}/${total}`);
-
+        console.log(`Progress: ${Math.min(offset, total)}/${total}`);
     } while (offset < total);
 
-    fs.writeFileSync('mangaIds.json', JSON.stringify(mangaIds, null, 2));
-    return mangaIds.map(manga => manga.id);
+    return mangaIds;
 }
 
-async function getMangaTitleById(id: string, token: string) {
+async function getMangaTitleById(id: string, token: string): Promise<string> {
     try {
-        const result = await axios.get(`https://api.mangadex.org/manga/${id}`, {
+        const result = await axios.get(`${CONFIG.MANGA_API}/manga/${id}`, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         });
 
-        // Get the title from the manga attributes
         let title = result.data.data.attributes.title.en;
-
-        // If English title is not available, use the first available title
         if (!title) {
             const titles = result.data.data.attributes.title;
             for (let lang in titles) {
@@ -69,39 +73,49 @@ async function getMangaTitleById(id: string, token: string) {
         return title;
     } catch (error) {
         console.error(`Error occurred while fetching title for manga with id ${id}:`, error);
+        return 'Title not found';
     }
 }
 
-async function fetchAllTitles(token: string, ids: string[]) {
-    const titles: { [id: string]: string } = {};
+async function fetchAllTitles(mangas: Manga[]): Promise<Record<string, string>> {
+    const titles: Record<string, string> = {};
 
-    for (const id of ids) {
-        const title = await getMangaTitleById(id, token);
-        titles[id] = title;
-        console.log(`Fetched title for manga with id ${id}: ${title}`);
+    for (const manga of mangas) {
+        titles[manga.id] = manga.title;
     }
 
-    fs.writeFileSync('mangaIdTitles.json', JSON.stringify(titles, null, 2));
+    writeToFile('mangaIdTitles.json', titles);
     return titles;
 }
 
-async function generateFeedUrls(ids: string[], titles: { [id: string]: string }) {
-    const feedUrls = [];
-    for (let i = 0; i < ids.length; i += 10) {
-        const mangaIds = ids.slice(i, i + 10);
-        const queries = mangaIds.map((id: string) => `q=manga:${id},tl:${language}`).join('&');
-        const url = `${baseUrl}${queries}`;
-        feedUrls.push({ url, titles: mangaIds.map(id => titles[id]) });
+function generateFeedUrls(mangas: Manga[], titles: Record<string, string>): void {
+    const feedUrls: {url: string, titles: string[]}[] = []; // Fixed here
+    let feedUrlsList = '';
+    for (let i = 0; i < mangas.length; i += 10) {
+        const mangaSlice = mangas.slice(i, i + 10);
+        const queries = mangaSlice.map(manga => `q=manga:${manga.id},tl:${CONFIG.DEFAULT_LANGUAGE}`).join('&');
+        const url = `${CONFIG.BASE_URL}${queries}`;
+        feedUrls.push({ url, titles: mangaSlice.map(manga => titles[manga.id]) });
+        feedUrlsList += url + '\n';
     }
-    fs.writeFileSync('feedUrls.json', JSON.stringify(feedUrls, null, 2));
+    writeToFile('feedUrls.json', feedUrls);
+    writeToFile('feedUrlsList.txt', feedUrlsList, false);
 }
 
-authenticate()
-    .then(({ token, refreshToken }) => {
-        return getAllMangaId(token, refreshToken).then((ids) => {
-            return fetchAllTitles(token, ids).then((titles) => {
-                generateFeedUrls(ids, titles);
-            });
-        });
-    })
-    .catch(console.error);
+
+function writeToFile(fileName: string, data: any, stringify: boolean = true): void {
+    fs.writeFileSync(fileName, stringify ? JSON.stringify(data, null, 2) : data);
+}
+
+async function start(): Promise<void> {
+    try {
+        const { token, refreshToken } = await authenticate();
+        const mangas = await getAllMangaId(token, refreshToken);
+        const titles = await fetchAllTitles(mangas);
+        generateFeedUrls(mangas, titles);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
