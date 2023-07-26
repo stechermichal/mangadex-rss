4d3297fb83184b19dba3b7b7a5d16d24eb450481
Michal Štecher <m.stecher@cleverbee.com>
2023-07-26 11:55:42 +0200

    fix: remove npm version check, add calls to make the commands wait for their turn

--- run.bat
+++ run.bat
@@ -8,16 +8,9 @@ IF ERRORLEVEL 1 (
     EXIT /B 1
 )
 
-:: Check if npm is installed
-npm -v >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo npm is not installed. Please install it from https://nodejs.org/ and run this script again.
-    EXIT /B 1
-)
-
 :: Install necessary packages
 echo Installing necessary packages...
-npm install
+call npm install
 IF ERRORLEVEL 1 (
     echo An error occurred while installing the packages.
     EXIT /B 1
@@ -25,7 +18,7 @@ IF ERRORLEVEL 1 (
 
 :: Compile TypeScript to JavaScript
 echo Compiling TypeScript to JavaScript...
-npx tsc
+call npx tsc
 IF ERRORLEVEL 1 (
     echo An error occurred while compiling TypeScript.
     EXIT /B 1
